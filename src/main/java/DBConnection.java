9f5e2644a3ec2a457a46f7860e60cc825c1b49e1
Prashant Deepake <deepakeprashant1@gmail.com>
2023-03-13 20:40:02 +0530

    Added: Connect java program to database

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBConnection {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "root";
+        Connection conn;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        }catch (ClassNotFoundException e){
+            throw new IllegalStateException("Cannot find the driver in the classpath",e);
+        }
+        listDrivers();
+
+        try {
+            System.out.println("Connecting to database"+jdbcUrl);
+            conn = DriverManager.getConnection(jdbcUrl,userName,password);
+            System.out.println("Connection Successfully");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+
+        }
+    }
+}
